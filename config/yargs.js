74c118d289b98816b66042a62bc84352abe21aad
grelacode <32378395+grelacode@users.noreply.github.com>
2020-10-26 15:57:37 -0300

    Update yargs.js

--- config/yargs.js
+++ config/yargs.js
@@ -1,32 +1,31 @@
+'use strict';
+
 const description = {
     demand: true,
     alias: 'd',
-    desc: 'descripcion de la tarea por hacer'
+    desc: 'Task description'
 };
 
 const completed = {
     default: true,
     alias: 'c',
-    desc: 'marca como completado o pepndiente una tarea'
+    desc: 'Tick a completed task'
 };
 
 const argv = require('yargs')
-    .command('create', 'agrega una nueva tarea a la lista', {
+    .command('create', 'Add a new task in the list', {
         description
     })
-    .command('update', 'actualiza el estado completado de una tarea', {
+    .command('update', 'Update an task state', {
         description,
         completed
     })
-    .command('delete', 'borra una tarea de mi lista', {
+    .command('delete', 'Delete one task from the list', {
         description
     })
 
 .help()
 .argv;
-
-
-
 
 module.exports = {
     argv
